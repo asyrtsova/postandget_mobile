fa904ec7f2ddc03bd4d24b30b961606df47470be
ALINA SYRTSOVA <alinasyrtsova@gmail.com>
2017-04-27 20:44:44 -0700

    first draft, based on homephone

---
+++ app/src/main/java/com/ucsc/alina/postandget/MainActivity.java
@@ -0,0 +1,73 @@
+package com.ucsc.alina.postandget;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = "PostAndGet";
+
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        queue = Volley.newRequestQueue(this);
+    }
+
+    private void getRequest(){
+
+    }
+
+    private void postRequest() {
+
+        StringRequest sr = new StringRequest(DownloadManager.Request.Method.POST,
+                "https://luca-ucsc-teaching-backend.appspot.com/hw3/request_via_post" ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(LOG_TAG, "Got:" + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("sender", sender);
+                params.put("recipient" , recipient);
+                params.put("msg", msg);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("Content-Type","application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+        queue.add(sr);
+
+    }
+}
